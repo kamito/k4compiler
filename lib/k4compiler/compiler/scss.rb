bac1f40d49105b75fc2d4eab0e6b853d42a7b1a9
Shinichirow Kamito <kamito@debian.416.bz>
2013-07-13 14:29:55 +0900

    Impremented SCSS compiler.

--- lib/k4compiler/compiler/scss.rb
+++ lib/k4compiler/compiler/scss.rb
@@ -1,4 +1,23 @@
+
+require 'sass'
+
 module K4compiler
   class Scss < Base
+
+    # compile
+    def compile(src)
+      options = {
+        :syntax => config.syntax,
+        :style => config.style,
+        :load_paths => config.load_paths,
+      }
+      engine = ::Sass::Engine.new(src, options)
+      compiled = engine.render()
+      return compiled.chomp
+    end
+
+    def config
+      return super.scss
+    end
   end
 end
--- spec/k4compiler/compiler/scss_spec.rb
+++ spec/k4compiler/compiler/scss_spec.rb
@@ -13,4 +13,58 @@ describe K4compiler::Scss do
       @compiler.should be_instance_of(K4compiler::Scss)
     end
   end
+
+  describe "#compile" do
+    it "should make compressed css" do
+      config = K4compiler::Config.new
+      compiler = K4compiler::Scss.new(config)
+      scss = <<__SCSS__
+body {
+    background-color: #000000;
+    color: #FFFFFF;
+    font-size: 10pt;
+    #contents {
+        background-color: #FFFFFF;
+        color: #000000;
+    }
+}
+__SCSS__
+      result_css = "body{background-color:#000000;color:#FFFFFF;font-size:10pt}body #contents{background-color:#FFFFFF;color:#000000}"
+      compiler.compile(scss).should eq(result_css)
+    end
+
+    it "should make compact css" do
+      config = K4compiler::Config.new
+      config.scss.style = :compact
+      compiler = K4compiler::Scss.new(config)
+      scss = <<__SCSS__
+body {
+    background-color: #000000;
+    color: #FFFFFF;
+    font-size: 10pt;
+    #contents {
+        background-color: #FFFFFF;
+        color: #000000;
+    }
+}
+__SCSS__
+      result_css = <<__RESULT__
+body { background-color: #000000; color: #FFFFFF; font-size: 10pt; }
+body #contents { background-color: #FFFFFF; color: #000000; }
+__RESULT__
+      compiler.compile(scss).should eq(result_css.chomp)
+    end
+
+    it "should make imported css" do
+      sample_dir = File.expand_path(File.join(File.dirname(__FILE__), 'scss'))
+
+      config = K4compiler::Config.new
+      config.scss.load_paths << sample_dir
+      compiler = K4compiler::Scss.new(config)
+
+      scss = File.read(File.join(sample_dir, 'sample.scss'))
+      result_css = "body{background-color:#000000;color:#FFFFFF;font-size:10pt}body #contents{background-color:#FFFFFF;color:#000000}"
+      compiler.compile(scss).should eq(result_css.chomp)
+    end
+  end
 end
